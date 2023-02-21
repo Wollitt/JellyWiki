8466c1365e025da5f22b2d2380b3f93e572d4b06
Adam Piórkowski <58884846+Wollitt@users.noreply.github.com>
2023-02-21 12:53:43 +0100

    Add files via upload

---
+++ src/main/java/Jelly/Wollit/JellyWiki/JellyWikiApplication.java
@@ -0,0 +1,13 @@
+package Jelly.Wollit.JellyWiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellyWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jelly/Wollit/JellyWiki/JellyWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package Jelly.Wollit.JellyWiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
